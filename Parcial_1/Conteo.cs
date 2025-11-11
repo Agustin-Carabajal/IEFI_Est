87dff107a7c5a3ea6d261ec5a8fb2aead4492992
Agustin-Carabajal <carabajal.agustin.ezequiel@gmail.com>
2025-11-10 21:04:01 -0300

    Agregar archivos de proyecto.

---
+++ Parcial_1/Conteo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IEFI_Est
+{
+    internal class Conteo
+    {
+        public int Factorial(int numero)
+        {
+            if (numero < 0)
+            {
+                throw new ArgumentException("El número debe ser no negativo.");
+            }
+            if (numero == 0 || numero == 1)
+            {
+                return 1;
+            }
+            int resultado = 1;
+            for (int i = 2; i <= numero; i++)
+            {
+                resultado *= i;
+            }
+            return resultado;
+        }
+
+        public int Combinacion(int n, int k)
+        {
+            if (k < 0 || n < 0 || k > n)
+            {
+                throw new ArgumentException("Los valores de n y k deben ser no negativos y k no debe ser mayor que n.");
+            }
+            return Factorial(n) / (Factorial(k) * Factorial(n - k));
+        }
+
+        public int Permutacion(int n, int k)
+        {
+            if (k < 0 || n < 0 || k > n)
+            {
+                throw new ArgumentException("Los valores de n y k deben ser no negativos y k no debe ser mayor que n.");
+            }
+            return Factorial(n) / Factorial(n - k);
+        }
+    }
+}
---
+++ Parcial_1/MMM_Datos_Agrupados.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEFI_Est
+{
+    internal class MMM_Datos_Agrupados
+    {
+        public decimal Moda(decimal limiteInferior, decimal frecAbsInterModal, decimal frecAbsInterModalAnterior,
+                                decimal frecAbsInterModalPosterior, decimal amplitudIntervalo)
+        {
+            decimal moda = limiteInferior + ((frecAbsInterModal - frecAbsInterModalAnterior) / 
+                ((frecAbsInterModal - frecAbsInterModalAnterior) + (frecAbsInterModal - frecAbsInterModalPosterior))) * amplitudIntervalo;
+            return moda;
+        }
+
+        public decimal Media(decimal limiteInferior, decimal frecuenciaAbsAcumulada,
+                                 decimal frecAcumuladaInterModalAnterior, decimal frecuenciaAbsInterMedia,
+                                 decimal amplitudIntervalo)
+        {
+            decimal media = limiteInferior + 
+                            (((frecuenciaAbsAcumulada/2) - frecAcumuladaInterModalAnterior) / 
+                                frecuenciaAbsInterMedia) * 
+                                amplitudIntervalo;
+
+            return media;
+        }
+
+    }
+}
